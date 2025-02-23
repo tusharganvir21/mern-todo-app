2b59dac0fbbc12e4c0419f3f6802ac9a43d1467b
unknown <tushar.ganvir21@vit.edu>
2025-02-23 09:58:41 +0530

    update code

--- todoList/src/Create.jsx
+++ todoList/src/Create.jsx
@@ -7,7 +7,7 @@ function Create( {onTaskAdded} ) {
     const handleAdd = () => {
       if(!task.trim()) return;
 
-        axios.post(`${import.meta.env.BACKEND_URL}/add`, {task: task})
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/add`, {task: task})
         .then(result => {
           // location.reload()   
            onTaskAdded(result.data);
--- todoList/src/Home.jsx
+++ todoList/src/Home.jsx
@@ -1,69 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import Create from './Create'
+import React, { useEffect, useState } from 'react';
+import Create from './Create';
 import axios from 'axios';
 import { BsFillCheckCircleFill, BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 function Home() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-      axios.get(`${import.meta.env.BACKEND_URL}/get`)
-      .then(result => setTodos(result.data))
-      .catch(err => console.log(err)
-      )
-    },[]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/get`)
+      .then((result) => {
+        console.log(result.data);
+        setTodos(result.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const handleEdit = (id) => {
-      axios.put(`${import.meta.env.BACKEND_URL}/update/`+id)
-      .then(result => {
-        // location.reload()
-        setTodos(prevTodos =>
-          prevTodos.map(todo => 
-            todo._id === id ? {...todo, done: !todo.done} : todo
+  const handleEdit = (id) => {
+    axios
+      .put(`${import.meta.env.VITE_BACKEND_URL}/update/` + id)
+      .then((result) => {
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo._id === id ? { ...todo, done: !todo.done } : todo
           )
         );
       })
-      .catch(err => console.log(err))
-    }
+      .catch((err) => console.log(err));
+  };
 
-    const handleDelete = (id) => {
-      axios.delete(`${import.meta.env.BACKEND_URL}/delete/`+id)
-      .then(result => {
-        // location.reload()
-        setTodos(prevTodos => prevTodos.filter(todo => todo._id !== id));
+  const handleDelete = (id) => {
+    axios
+      .delete(`${import.meta.env.VITE_BACKEND_URL}/delete/` + id)
+      .then((result) => {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
       })
-      .catch(err => console.log(err))
-    }
+      .catch((err) => console.log(err));
+  };
 
-    return (
+  return (
     <>
       <div className="home">
-        <h2 className='heading'>Todo List</h2>
-        <Create onTaskAdded={(newTask) => setTodos((prevTodos) => [...prevTodos, newTask])} /> 
-        {
-          todos.length === 0
-          ? <div><h2 className='no-record'>No Records</h2></div>
-          : todos.map((todo, index) => (
-              <div className='task' key={index}>
-                <div className="checkbox" onClick={() => handleEdit(todo._id)}>
-                  {todo.done 
-                  ? <BsFillCheckCircleFill className="icon"/>
-                  : <BsCircleFill className="icon" />
-                  }
-                  <p className={todo.done ? 'line-through' : ''}>{todo.task}</p>
-                </div>
-                <div>
-                  <span><BsFillTrashFill className='icon' onClick={() => handleDelete(todo._id)}/></span>
-                </div>
+        <h2 className="heading">Todo List</h2>
+        <Create
+          onTaskAdded={(newTask) => setTodos((prevTodos) => [...prevTodos, newTask])}
+        />
+        {todos.length === 0 ? (
+          <div>
+            <h2 className="no-record">No Records</h2>
+          </div>
+        ) : (
+          todos.map((todo, index) => (
+            <div className="task" key={index}>
+              <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+                {todo.done ? (
+                  <BsFillCheckCircleFill className="icon" />
+                ) : (
+                  <BsCircleFill className="icon" />
+                )}
+                <p className={todo.done ? 'line-through' : ''}>{todo.task}</p>
               </div>
+              <div>
+                <span>
+                  <BsFillTrashFill
+                    className="icon"
+                    onClick={() => handleDelete(todo._id)}
+                  />
+                </span>
+              </div>
+            </div>
           ))
-        }
+        )}
       </div>
-        <footer className="footer">
-          <p>Created by <span className="footer-name">Tushar Ganvir</span></p>
-        </footer>
+      <footer className="footer">
+        <p>
+          Created by <span className="footer-name">Tushar Ganvir</span>
+        </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
